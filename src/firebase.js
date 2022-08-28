845273855648053367f47a83d010c9130cfe42fe
Alex Nicolaou <alex@stockgamblers.com>
2022-08-28 09:52:56 -0400

    npm run format

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjB6iJwlZxMlUjS_Q91JdK0yPA-vRljDU",
-  authDomain: "blueroux-5772a.firebaseapp.com",
-  projectId: "blueroux-5772a",
-  storageBucket: "blueroux-5772a.appspot.com",
-  messagingSenderId: "307152117266",
-  appId: "1:307152117266:web:b1bce200fb687ba8ae02cb",
-  measurementId: "G-6C5J9P8BQL"
+	apiKey: 'AIzaSyAjB6iJwlZxMlUjS_Q91JdK0yPA-vRljDU',
+	authDomain: 'blueroux-5772a.firebaseapp.com',
+	projectId: 'blueroux-5772a',
+	storageBucket: 'blueroux-5772a.appspot.com',
+	messagingSenderId: '307152117266',
+	appId: '1:307152117266:web:b1bce200fb687ba8ae02cb',
+	measurementId: 'G-6C5J9P8BQL'
 };
 
 // Initialize Firebase
