a9217564dbda68ab0cbe5ea2874b0fd8e28c82dc
Alex Nicolaou <alex@stockgamblers.com>
2022-08-28 20:57:23 -0400

    Write a user record into firestore on signin.

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,6 +1,7 @@
 <script>
 	import firebase from '../firebase';
 	import { store } from '../store';
+	import { collection, addDoc } from 'firebase/firestore';
 
 	const auth = firebase.auth;
 	const gAuthProvider = firebase.google_auth_provider;
@@ -18,20 +19,28 @@
 					authMessage: ''
 				})
 			);
-			// ...
+			addDoc(collection(firebase.firestore, 'users'), {
+				name: user.displayName,
+				email: user.email,
+				photo: user.photoURL,
+				activity_timestamp: new Date().getTime()
+			}).catch((message) => {
+				// TODO: Surface this error state in the UI.
+				console.error(message);
+			});
 		} else {
-            store.dispatch(signed_out());
+			store.dispatch(signed_out());
 		}
 	});
 
 	function signin() {
 		signInWithPopup(auth, gAuthProvider).catch((message) => {
-            store.dispatch(error(message))
+			store.dispatch(error(message));
 		});
 	}
 	function signout() {
 		signOut(auth).catch((message) => {
-            store.dispatch(error(message))
+			store.dispatch(error(message));
 		});
 	}
 </script>
@@ -39,7 +48,7 @@
 {#if $store.auth.signedIn !== true}
 	<button on:click={signin}>Sign In</button>
 {:else}
-	<p><img src={$store.auth.photo} referrerpolicy="no-referrer"/>{$store.auth.email}</p>
-    <p>{$store.auth.name}</p>
+	<p><img src={$store.auth.photo} referrerpolicy="no-referrer" />{$store.auth.email}</p>
+	<p>{$store.auth.name}</p>
 	<button on:click={signout}>Sign Out</button>
 {/if}
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 //import { getAnalytics } from 'firebase/analytics';
 //
 // TODO: Add SDKs for Firebase products that you want to use
@@ -26,6 +27,7 @@ const firebase = {
 	app: initializeApp(firebaseConfig),
 	auth: getAuth(),
 	google_auth_provider: new GoogleAuthProvider(),
+	firestore: getFirestore()
 };
 
 export default firebase;
