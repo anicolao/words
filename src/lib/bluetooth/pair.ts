01fd2437caea757c8d11cafdac28cdcfc2f1d24a
Alex Nicolaou <alex@stockgamblers.com>
2022-09-05 23:18:34 -0400

    Auto reconnect and characteristic info for GAN

--- src/lib/bluetooth/pair.ts
+++ src/lib/bluetooth/pair.ts
@@ -1,7 +1,8 @@
 export async function pair(): BluetoothRemoteGATTServer {
   let device;
   try {
-    device = await navigator.bluetooth.requestDevice( { acceptAllDevices: true } );
+    device = await navigator.bluetooth.requestDevice( { optionalServices: [ '0000180a-0000-1000-8000-00805f9b34fb', '0000fff0-0000-1000-8000-00805f9b34fb' ], acceptAllDevices: true } );
+    //device = await navigator.bluetooth.requestDevice( { acceptAllDevices: true } );
   } catch (e) {
     throw new Error(e);
   }
@@ -11,6 +12,61 @@ export async function pair(): BluetoothRemoteGATTServer {
   console.log("Server:", server);
 
 	return server;
+}
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
+}
+
+export async function listKnownDevices() {
+  const devices = await navigator.bluetooth.getDevices();
+  console.log(devices);
+  if (devices.length === 0) {
+    return false;
+  }
+	for (const device of devices) {
+		const abortController = new AbortController();
+		const log = console.log;
+		log("listen for ads from ", device);
+		device.addEventListener(
+			"advertisementreceived",
+			() => {
+				log("ad: ", device);
+				abortController.abort();
+				device.gatt.connect()
+				.then(
+					server => { log('Connected: ', device); 
+						log('Getting Services...');
+						return server.getPrimaryServices();
+					})
+					.then(services => {
+						log('Getting Characteristics...');
+						let queue = Promise.resolve();
+						services.forEach(service => {
+							queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+								log('> Service: ' + service.uuid);
+								characteristics.forEach(characteristic => {
+									log('>> Characteristic: ' + characteristic.uuid + ' ' +
+											getSupportedProperties(characteristic));
+								});
+							}));
+						});
+						return queue;
+					})
+				  .catch((err) => { log('Error: ', err); });
+			},
+			{ once: true }
+		);
+		log("watchAdvertisements");
+		device.watchAdvertisements({ signal: abortController.signal });
+		log("done");
+	}
 }
 
 export default pair;
--- src/lib/components/Pair.svelte
+++ src/lib/components/Pair.svelte
@@ -1,6 +1,7 @@
 <script>
-	import pair from '$lib/bluetooth/pair';
+	import { pair, listKnownDevices } from '$lib/bluetooth/pair';
 	import Button from '@smui/button';
 </script>
 
 <Button on:click={pair} variant="raised">Pair</Button>
+<Button on:click={listKnownDevices} variant="raised">List</Button>
