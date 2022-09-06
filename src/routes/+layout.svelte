b78d8618dbf190927dbc65a40be656ce6e3ce948
Alex Nicolaou <alex@stockgamblers.com>
2022-09-06 18:47:37 -0400

    Attempt to use material top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,47 @@
-<script>
-	import '../app.postcss';
+<TopAppBar bind:this={topAppBar} variant="fixed">
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Fixed</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+  <slot />
+</AutoAdjust>
+
+<script lang="ts">
+  import type { TopAppBarComponentDev } from '@smui/top-app-bar';
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+
+  let topAppBar: TopAppBarComponentDev;
 </script>
 
-<slot />
+<style>
+  /* Hide everything above this component. */
+  :global(app),
+  :global(body),
+  :global(html) {
+    display: block !important;
+    height: auto !important;
+    width: auto !important;
+    position: static !important;
+  }
+</style>
