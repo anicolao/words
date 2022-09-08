bd8605ac40b1bdc9cca9b5237dfc5814941c6171
Alex Nicolaou <alex@stockgamblers.com>
2022-09-08 09:22:02 -0400

    Fix typescript errors

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -3,28 +3,28 @@
 </Content>
 
 <script lang="ts">
-  import { Content, } from '@smui/card';
-  import { connectSmartRobot } from "cubing/bluetooth";
-  import { randomScrambleForEvent } from "cubing/scramble";
-  import "cubing/twisty";
-  import { onMount } from 'svelte';
-	import { TwistyPlayer } from "cubing/twisty";
+	import { Content } from '@smui/card';
+	import { connectSmartRobot } from 'cubing/bluetooth';
+	import { randomScrambleForEvent } from 'cubing/scramble';
+	import 'cubing/twisty';
+	import { onMount } from 'svelte';
+	import { TwistyPlayer } from 'cubing/twisty';
 
-  let robot;
-  let scramble;
+	let robot;
+	let scramble: ReturnType<typeof randomScrambleForEvent>;
 
-  onMount(async () => {
-		scramble = await randomScrambleForEvent("333");
+	onMount(async () => {
+		scramble = randomScrambleForEvent('333');
 
-		let contentElem = document.querySelector("#main-content");
-		const twistyPlayer: TwistyPlayer = new TwistyPlayer();
-		twistyPlayer.background = "none";
-		twistyPlayer.visualization = "PG3D";
-		twistyPlayer.controlPanel = "none";
-		twistyPlayer.backView = "top-right";
-		twistyPlayer.hintFacelets = "none";
-		contentElem.appendChild(twistyPlayer);
+		let contentElem = document.querySelector('#main-content');
+		if (contentElem) {
+			const twistyPlayer: TwistyPlayer = new TwistyPlayer();
+			twistyPlayer.background = 'none';
+			twistyPlayer.visualization = 'PG3D';
+			twistyPlayer.controlPanel = 'none';
+			twistyPlayer.backView = 'top-right';
+			twistyPlayer.hintFacelets = 'none';
+			contentElem.appendChild(twistyPlayer);
+		}
 	});
 </script>
-
-
