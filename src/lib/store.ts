8b204f501dbef43e1a96f242965ce1858907e650
Alex Nicolaou <alex@stockgamblers.com>
2022-09-08 09:07:21 -0400

    Type store correctly.

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,21 @@
-//import { combineReducers, configureStore, createStore } from '@reduxjs/toolkit';
 import * as toolkitRaw from '@reduxjs/toolkit';
-const { combineReducers, configureStore, createStore } = ((toolkitRaw as any).default ?? toolkitRaw) as typeof toolkitRaw;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const { configureStore } = ((toolkitRaw as any).default ?? toolkitRaw) as typeof toolkitRaw;
 import { auth } from './components/auth';
 import { cubes } from '$lib/components/cubes';
 import { nav } from '$lib/components/nav';
+import type { Writable } from 'svelte/store';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function svelteStoreEnhancer(createStoreApi: (arg0: any, arg1: any) => any) {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	return function (reducer: any, initialState: any) {
 		const reduxStore = createStoreApi(
 			reducer, initialState
 		);
 		return {
 			...reduxStore,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			subscribe(fn: (arg0: any) => void) {
 				fn(reduxStore.getState());
 
@@ -28,4 +32,10 @@ const reducer = {
     cubes,
     nav,
 }
-export const store = configureStore({ reducer, enhancers: [svelteStoreEnhancer] });
+const rawStore = configureStore({ reducer, enhancers: [svelteStoreEnhancer] });
+export type ReduxStore = typeof rawStore;
+export type GlobalState = ReturnType<typeof rawStore.getState>;
+type SvelteStore = Writable<GlobalState>;
+
+
+export const store = rawStore as ReduxStore & SvelteStore;
