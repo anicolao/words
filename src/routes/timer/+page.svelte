2967b3c0767df0d1ae1a9c536ee3e6c707f6a290
Alex Nicolaou <alex@stockgamblers.com>
2022-09-15 17:49:32 -0400

    New solve detectino produces tighter times.

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -3,14 +3,13 @@
 	import { randomScrambleForEvent } from 'cubing/scramble';
 	import 'cubing/twisty';
 	import { onMount, onDestroy } from 'svelte';
-	import { TreeAlgIndexer, TwistyAlgEditor, TwistyPlayer } from 'cubing/twisty';
+	import { TwistyPlayer } from 'cubing/twisty';
 	import { store } from '$lib/store';
 	import type { CubeInfo } from '$lib/components/cubes';
 	import { GANCube } from '$lib/bluetooth/gan/gan356i_v1';
 	import { Alg, Move } from 'cubing/alg';
 	import { experimentalAppendMove } from '$lib/cubing/alg/operation';
 	import type { KPuzzle } from 'cubing/kpuzzle';
-	import { startAfter } from 'firebase/firestore';
 	import Pair from '$lib/components/Pair.svelte';
 
 	let scramble = new Alg();
@@ -39,15 +38,11 @@
 		}
 	});
 
-	let moveCount = 0;
-	let version = '_unknown';
 	let currentDevice: CubeInfo | undefined;
 	let cube: GANCube | undefined;
 	let alg = new Alg();
 	let startWhenReady = false;
 	let solving = false;
-	let started = new Date();
-	let ended = new Date();
 
 	async function addMove(model: any, move: string) {
 		alg = experimentalAppendMove(alg, new Move(move), {
@@ -55,21 +50,25 @@
 			wideMoves333: true,
 			sliceMoves333: true
 		});
-		console.log(alg);
 		let inverted = Array.from(alg.invert().childAlgNodes());
 		inverted = inverted.concat(Array.from(new Alg(scramble).childAlgNodes()));
 		remaining = new Alg(inverted).simplify({ collapseMoves: true, quantumMoveOrder: () => 4 });
-		console.log(`remaining: [${remaining}]`);
 		if (remaining.isIdentical(new Alg()) && !solving) {
 			startWhenReady = true;
 		} else if (solving) {
-			ended = new Date();
 		} else if (startWhenReady) {
 			solving = true;
 			cube?.setTrackingRotations(true);
-			started = new Date();
-			ended = new Date();
 			startWhenReady = false;
+		}
+
+		if (solving) {
+			const kp: KPuzzle = await twistyPlayer.experimentalModel.kpuzzle.get();
+			const check = kp.startState().applyAlg(alg);
+			isSolved = check.experimentalIsSolved({
+				ignorePuzzleOrientation: true,
+				ignoreCenterOrientation: true
+			});
 		}
 
 		// defer update until after transforms
@@ -87,13 +86,7 @@
 				move: lastMove,
 				amount: 0
 			});
-			viewerAlg = alg;
 			model.alg.set(alg);
-			const state = await model.currentState.get();
-			isSolved = state.experimentalIsSolved({
-				ignorePuzzleOrientation: true,
-				ignoreCenterOrientation: true
-			});
 		}, 100);
 	}
 	$: if ($store.cubes.connectedDevice !== currentDevice) {
@@ -101,13 +94,10 @@
 			currentDevice = $store.cubes.connectedDevice;
 			if (currentDevice) {
 				cube = new GANCube({ id: currentDevice[0] });
-				version = await cube.getVersionAsString();
 				const model = twistyPlayer.experimentalModel;
-				const kstate = await model.currentState.get();
 				const kp: KPuzzle = await twistyPlayer.experimentalModel.kpuzzle.get();
 				const ksNew = kp.startState();
 				cube.watchMoves((move: number) => {
-					moveCount++;
 					const face = GANCube.colorToFaceMove(move, ksNew.applyAlg(alg).stateData);
 					addMove(model, face);
 				});
@@ -116,8 +106,7 @@
 		useCurrentDevice();
 	}
 
-	let viewerAlg = new Alg();
-	let isSolved = true;
+	let isSolved = false;
 
 	$: remainingNodes = Array.from(remaining.childAlgNodes());
 	$: algView = Array.from(scramble.childAlgNodes()).map((x, i, a) => {
@@ -150,6 +139,12 @@
 			rafTimer = rafTime - rafStart;
 		});
 	}
+	$: if (isSolved) {
+		console.log("***** SOLVED *****");
+		console.log({ rafTimer, timerInTenths, alg, moves: `[${alg.toString()}]` })
+		console.log("******************")
+	}
+
 	$: timerInTenths = Math.round(rafTimer / 100);
 	$: timerSecs = Math.floor(timerInTenths / 10);
 	$: timerTenths = timerInTenths % 10;
